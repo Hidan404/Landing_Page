93d74bd400e5f35dfe7847e719db5b7f90c67020
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-30 22:41:09 -0300

    add todo functionality with task management; update task_app.html and todo_list.html templates; enhance static.css for improved styling

--- App/views.py
+++ App/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for
 from App import app
 
+tarefas = []
 
 @app.route('/')
 def index():
@@ -14,3 +15,18 @@ def static_files(path):
 def task():
     return render_template('task_app.html')
 
+@app.route("/todo",methods=['GET', 'POST'])
+def todo():
+    if request.method == 'POST':
+        nova_tarefa = request.form.get('tarefa')
+        if nova_tarefa:
+            tarefas.append(nova_tarefa)
+    return render_template('todo_list.html', tarefas=tarefas)
+
+
+
+@app.route("/delete/<int:tarefa_id>")
+def delete(tarefa_id):
+    if 0 <= tarefa_id < len(tarefas):
+        tarefas.pop(tarefa_id)
+    return render_template('todo_list.html', tarefas=tarefas)
